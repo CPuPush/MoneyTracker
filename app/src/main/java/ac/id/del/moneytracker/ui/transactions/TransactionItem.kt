104289093f55b743e84fc7cf2df31faf4dc9ec50
patriciamys <patriciamys23@gmail.com>
2022-05-26 00:56:20 +0700

    [MoneyTracker][Fitur Transaction] Making Data for Transaction

---
+++ app/src/main/java/ac/id/del/moneytracker/ui/transactions/TransactionItem.kt
@@ -0,0 +1,8 @@
+package ac.id.del.moneytracker.ui.transactions
+
+data class TransactionItem(
+    val transactionDate: String?="",
+    val transactionDay: String?="",
+    val transactionYearMonth: String?="",
+    val transactionTotal: String?=""
+)
