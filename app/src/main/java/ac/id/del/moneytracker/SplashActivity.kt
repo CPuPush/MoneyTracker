9f7e444d0b6cb44b405fda46078378fbbd603985
CPuPush <fpakpahan44@gmail.com>
2022-05-25 11:01:58 +0700

    splash

--- app/src/main/java/ac/id/del/moneytracker/SplashActivity.kt
+++ app/src/main/java/ac/id/del/moneytracker/SplashActivity.kt
@@ -12,6 +12,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
 
         // kode untuk menjalankan main screen setelah time splash screen habis
         Handler().postDelayed({
