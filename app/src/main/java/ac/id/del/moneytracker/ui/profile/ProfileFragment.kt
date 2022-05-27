5347134dad50fd0366e91d55d7337dfccd3992ec
CPuPush <fpakpahan44@gmail.com>
2022-05-27 12:40:42 +0700

    profile

--- app/src/main/java/ac/id/del/moneytracker/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ac/id/del/moneytracker/ui/profile/ProfileFragment.kt
@@ -7,10 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-
-
-private const val DIALOG_DATE = "DialogDate"
 
 class ProfileFragment : Fragment() {
 
